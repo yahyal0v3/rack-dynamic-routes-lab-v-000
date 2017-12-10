5d35201546aa91e503379da62dfe41b63a281b65
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:33:51 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -1,15 +1,9 @@
 class Item
   attr_accessor :name, :price
 
-  @@all = []
-
   def initialize(name,price)
     @name = name
     @price = price
     @@all << self
-  end
-
-  def self.all
-    @@all
   end
 end

52dc2d2b57688c8aa61f5a47e6bc8fb17e442522
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:33:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,11 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = Item.all.find {|i| i.name == item_name}
+      item = @@items.find {|i| i.name == item_name}
       if item != nil
         resp.write item.price
       else
+        resp.write "Item not found"
         resp.status = 400
       end
     else

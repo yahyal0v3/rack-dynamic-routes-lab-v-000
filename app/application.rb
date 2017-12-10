659513bb11b2312b72b42962d0da59d6e8214bcc
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:04:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       item = Item.all.detect {|instance| instance.name == item_name}
-      if item 
+      binding.pry
+      if item
         resp.write item.price
       else
         resp.write

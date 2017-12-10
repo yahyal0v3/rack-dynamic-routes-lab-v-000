62949778a8d5535956cfce27e7311d97a6c0e301
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 03:55:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,5 +4,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      item_mame = req.path.split(/items/).last
+      item = Item.all.detect {|instance| instance.name == item_name}
+      rep.write item.prics
   end
 end

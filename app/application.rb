938fba0345c6b14d934207f26cb7401292daae9e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:39:18 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = Item.all.find{|i| i.name == item_name}
+      if item != nil
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,15 @@
 class Item
   attr_accessor :name, :price
 
+  @@all = []
+
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end

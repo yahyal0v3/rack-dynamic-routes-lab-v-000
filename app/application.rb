8013798afc6e2392252629cae39e4a3810a553e1
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:30:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,6 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      binding.pry
       item_name = req.path.split("/items/").last
       item = Item.all.find {|i| i.name == item_name}
       if item != nil

2a266bedd176018b8afd7bd18ee341b79500f3fc
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 04:03:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       item = Item.all.detect {|instance| instance.name == item_name}
-      resp.write item.price
+      if item 
+        resp.write item.price
+      else
+        resp.write
+        resp.status = 400
+      end
     else
       resp.status = 404
     end
